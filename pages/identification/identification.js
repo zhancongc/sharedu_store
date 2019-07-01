51e90bf8210263fcf0c708c549e6d23c73ad962b
詹聪聪 <1227753320@qq.com>
2019-07-01 18:58:06 +0800

    add location page

--- pages/identification/identification.js
+++ pages/identification/identification.js
@@ -11,10 +11,75 @@ Page({
       navHeight: app.globalData.navHeight,
       pageTitle: '门店入驻'
     },
+    cityPicker: false,
+    storeName: "",
+    cityName: "",
+    detailAddress: "",
+    location: 'dddd',
+    areaList: {
+      province_list: {
+        110000: '北京市',
+        120000: '天津市'
+      },
+      city_list: {
+        110100: '北京市',
+        110200: '县',
+        120100: '天津市',
+        120200: '县'
+      },
+      county_list: {
+        110101: '东城区',
+        110102: '西城区',
+        110105: '朝阳区',
+        110106: '丰台区',
+        120101: '和平区',
+        120102: '河东区',
+        120103: '河西区',
+        120104: '南开区',
+        120105: '河北区',
+      }
+    }
   },
   backHandler: function () {
     console.log("返回主页");
     wx.navigateBack();
+  },
+  onTapCityName: function() {
+    var that = this;
+    that.setData({
+      cityPicker : true
+    })
+  },
+  onCancelCityName :function () {
+    var that = this;
+    that.setData({
+      cityPicker: false
+    })
+  },
+  onConfirmCityName: function (event) {
+    var that = this;
+    var cityData = event.detail.values;
+    that.setData({
+      cityPicker: false,
+      cityName: cityData[0].name + " " + cityData[1].name + " " + cityData[2].name
+    })
+  },
+  //获取位置
+  getLocation(e) {
+    var that = this;
+    wx.chooseLocation({
+      success: function (res) {
+        console.log(res)
+        console.log(res.address)
+        console.log(res.latitude)
+        console.log(res.longitude)
+        console.log(res.name)
+        var location = res.address
+        that.setData({
+          detailAddress: location
+        })
+      }
+    })
   },
   /**
    * Lifecycle function--Called when page load
